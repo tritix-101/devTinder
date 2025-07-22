1d00dff959dc35575ade7663ce6b6fa2af0360af
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-07-22 19:20:38 +0530

    Exploring get,post,delete api calls

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express= require("express");
 const app=express();
 const port=7777;
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Priyanka",lastName:"Kandula"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Dta fetched from the server successfully")
+})
+app.delete("/user",(req,res)=>{
+    res.send("delete call performed"); 
+})
 app.use("/test",(req,res)=>{
     res.send("welcome to devtinder");
 })
