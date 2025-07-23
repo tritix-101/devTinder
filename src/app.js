547861ae185ef21bd34908183068ade06942d53d
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-07-23 11:55:39 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
-const express= require("express");
-const app=express();
-const port=7777;
+const express = require("express");
+const app = express();
+const port = 7777;
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Priyanka",lastName:"Kandula"});
-})
+app.use(
+  "/user",
+  [(req, res,next) => {
+    console.log("1st response");
+    //res.send("Sending 1st response");
+    next();
+  },
+  (req, res,next) => {
+    console.log("2nd response");
+    //res.send("2nd response");
+    next();
+  },
+  (req,res,next)=>{
+    console.log("3rd response");
+    next();
+    //res.send("3rd response");
+  },
+  (req,res,next)=>{
+    console.log("4th response");
+    res.send("4th response");
+  }]
+);
 
-
-app.get("/user/:name",(req,res)=>{
-    console.log(req.params);
-    res.send("exploring routing");
-})
-
-app.listen(port,()=>{
-    console.log(`server is listenting to port:${port}`);
-})
+app.listen(port, () => {
+  console.log(`server is listenting to port:${port}`);
+});
