778430a33a35944fa3978872dacc06af65a48b3e
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-07-23 10:23:40 +0530

    Exploring Routing Topic

--- src/app.js
+++ src/app.js
@@ -2,20 +2,16 @@ const express= require("express");
 const app=express();
 const port=7777;
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstName:"Priyanka",lastName:"Kandula"});
 })
-app.post("/user",(req,res)=>{
-    res.send("Dta fetched from the server successfully")
-})
-app.delete("/user",(req,res)=>{
-    res.send("delete call performed"); 
-})
-app.use("/test",(req,res)=>{
-    res.send("welcome to devtinder");
-})
 
 
+app.get("/user/:name",(req,res)=>{
+    console.log(req.params);
+    res.send("exploring routing");
+})
 
 app.listen(port,()=>{
     console.log(`server is listenting to port:${port}`);
