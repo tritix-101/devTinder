c06a4efa0918a9c3da0ccb2767b56489b2469c2a
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-08-04 11:56:55 +0530

    dynamic data signup api

--- src/app.js
+++ src/app.js
@@ -5,23 +5,27 @@ const app = express();
 const port = 7777;
 
 
-
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-  const user=new User({
-    firstName:"Jon",
-    lastName:"Doe",
-    age:25,
-    email:"jon@doe123.com",
-    password:"1234",
-    gender:"Male"
-  })
+
+  //Dynamic signup api
+  const user=new User(req.body);
+  //console.log(user);
+  // const user=new User({
+  //   firstName:"Jon",
+  //   lastName:"Doe",
+  //   age:25,
+  //   email:"jon@doe123.com",
+  //   password:"1234",
+  //   gender:"Male"
+  // })
 
   try{
     await user.save();
     res.send("user created successfully");
   }
   catch(error){
-    res.status(404).send("User not created");
+    res.status(400).send("User not created");
   }
   
 })
